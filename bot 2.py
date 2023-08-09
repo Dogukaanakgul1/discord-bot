5f5b5d1ce2a1f537db05a5907ce8e3b284fe7154
Dogukaanakgul1 <139368081+Dogukaanakgul1@users.noreply.github.com>
2023-08-09 20:22:03 +0300

    Create bot 2.py

---
+++ bot 2.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from googletrans import Translator
+import random
+import os
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+pollMessage = [] 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command(pass_context = True)
+async def poll(ctx, txt):
+    global pollMessage
+    await delete_messages(ctx.message)
+    await ctx.send(txt)
+    pollMessage = ["True",ctx.message]
+    await ctx.add_reaction(":thumbsup:")
+    await ctx.add_reaction(":thumbsdown:")
+bot.run("MTEzMTI2NDA4MDU4MjI5OTczOQ.GLxwgx.b9NL4epUTWhZLq-NblF1PB37U3XiA8wt--GgeM")
